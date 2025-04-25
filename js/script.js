011285c33902f57c9dae5e56a00575e1f40d8e31
miguel garcia <pablogarciagarza67@gmail.com>
2025-04-25 12:12:08 -0600

    primersubida

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('.search-button').addEventListener('click', function() {
+    const searchContainer = document.querySelector('.search-container');
+    searchContainer.classList.toggle('active');
+  });
+});
